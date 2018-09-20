886123e13116a3e3baebf8804051ee1b64a5f18e
Sudharshan Srinivasan <sudharshan25@hotmail.com>
2018-09-20 23:14:30 +0530

    Base table for requested and unapproved + sort by feature

--- vadhyaronline-web/src/app/shared/admin/approve/event-category/event-category.component.ts
+++ vadhyaronline-web/src/app/shared/admin/approve/event-category/event-category.component.ts
@@ -28,6 +28,20 @@ export class EventCategoryComponent implements OnInit {
     });
   }
 
+  sort(sort: { key: string, value: string }): void {
+    this.loading = true;
+    const dataList = [...this.data];
+    const sortName = sort.key;
+    const sortValue = sort.value;
+    if (sortName && sortValue) {
+      this.data = dataList.sort((a, b) => (sortValue === 'ascend')
+        ? (a[sortName] > b[sortName] ? 1 : -1) : (b[sortName] > a[sortName] ? 1 : -1));
+    } else {
+      this.data = dataList;
+    }
+    this.loading = false;
+  }
+
   ngOnInit() {
     this.loadAllEventCategories();
   }
--- vadhyaronline-web/src/app/shared/admin/approve/event-type/event-type.component.ts
+++ vadhyaronline-web/src/app/shared/admin/approve/event-type/event-type.component.ts
@@ -27,6 +27,21 @@ export class EventTypeComponent implements OnInit {
     });
   }
 
+
+  sort(sort: { key: string, value: string }): void {
+    this.loading = true;
+    const dataList = [...this.data];
+    const sortName = sort.key;
+    const sortValue = sort.value;
+    if (sortName && sortValue) {
+      this.data = dataList.sort((a, b) => (sortValue === 'ascend')
+        ? (a[sortName] > b[sortName] ? 1 : -1) : (b[sortName] > a[sortName] ? 1 : -1));
+    } else {
+      this.data = dataList;
+    }
+    this.loading = false;
+  }
+
   ngOnInit() {
     this.loadEventTypes();
   }
