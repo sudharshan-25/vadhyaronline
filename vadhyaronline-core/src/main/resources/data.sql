518203f9ca5e97e2c7456a96bc7145d2ab7792d6
Sudharshan Srinivasan <sudharshan25@hotmail.com>
2018-09-26 23:59:48 +0530

    Moved to POSTGRES

--- vadhyaronline-core/src/main/resources/data.sql
+++ vadhyaronline-core/src/main/resources/data.sql
@@ -3,31 +3,31 @@ INSERT INTO role_master(role_id, role_name) VALUES (2, 'Vadhyar');
 INSERT INTO role_master(role_id, role_name) VALUES (3, 'Assistant');
 INSERT INTO role_master(role_id, role_name) VALUES (4, 'User');
 
-INSERT INTO event_category(event_category_name, approved) VALUES ('Homa', 1);
-INSERT INTO event_category(event_category_name, approved) VALUES ('Tharpanam', 1);
-INSERT INTO event_category(event_category_name, approved) VALUES ('Devasam', 1);
-INSERT INTO event_category(event_category_name, approved) VALUES ('Manthra', 1);
-INSERT INTO event_category(event_category_name, approved) VALUES ('Shloka', 1);
+INSERT INTO event_category(event_category_name, approved) VALUES ('Homa', TRUE );
+INSERT INTO event_category(event_category_name, approved) VALUES ('Tharpanam', TRUE );
+INSERT INTO event_category(event_category_name, approved) VALUES ('Devasam', TRUE );
+INSERT INTO event_category(event_category_name, approved) VALUES ('Manthra', FALSE );
+INSERT INTO event_category(event_category_name, approved) VALUES ('Shloka', FALSE );
 
-INSERT INTO event_type(event_category_id, event_type_name, event_type_desc, approved) VALUES (1 , 'Sudharshana Homam', '', 1);
-INSERT INTO event_type(event_category_id, event_type_name, event_type_desc, approved) VALUES (2 , 'Amavasya Tharpanam', '', 1);
-INSERT INTO event_type(event_category_id, event_type_name, event_type_desc, approved) VALUES (3 , 'Pithru Tharpanam', '', 1);
-INSERT INTO event_type(event_category_id, event_type_name, event_type_desc, approved) VALUES (3 , 'Mathru Tharpanam', '', 0);
-INSERT INTO event_type(event_category_id, event_type_name, event_type_desc, approved) VALUES (4 , 'Sandiyavandanam', '', 0);
-INSERT INTO event_type(event_category_id, event_type_name, event_type_desc, approved) VALUES (5 , 'Bhagavad Gita', '', 1);
+INSERT INTO event_type(event_category_id, event_type_name, event_type_desc, approved) VALUES (1 , 'Sudharshana Homam', '', TRUE );
+INSERT INTO event_type(event_category_id, event_type_name, event_type_desc, approved) VALUES (2 , 'Amavasya Tharpanam', '', FALSE );
+INSERT INTO event_type(event_category_id, event_type_name, event_type_desc, approved) VALUES (3 , 'Pithru Tharpanam', '', TRUE );
+INSERT INTO event_type(event_category_id, event_type_name, event_type_desc, approved) VALUES (3 , 'Mathru Tharpanam', '', FALSE );
+INSERT INTO event_type(event_category_id, event_type_name, event_type_desc, approved) VALUES (4 , 'Sandiyavandanam', '', TRUE );
+INSERT INTO event_type(event_category_id, event_type_name, event_type_desc, approved) VALUES (5 , 'Bhagavad Gita', '', TRUE);
 
 INSERT INTO veda_master (veda_id, veda_name ) VALUES (1, 'Rig Veda');
 INSERT INTO veda_master (veda_id, veda_name ) VALUES (2, 'Yajur Veda');
 INSERT INTO veda_master (veda_id, veda_name ) VALUES (3, 'Sama Veda');
 INSERT INTO veda_master (veda_id, veda_name ) VALUES (4, 'Atharvana Veda');
 
-INSERT INTO soothram_master (soothram_name, approved, APPROVED_BY, REQUESTED_BY) VALUES ('Thrahyayana Soothram', 1, 1 ,2);
-INSERT INTO soothram_master (soothram_name, approved, APPROVED_BY, REQUESTED_BY) VALUES ('Bhodhayana Soothram', 1, 1 ,2);
-INSERT INTO soothram_master (soothram_name, approved, APPROVED_BY, REQUESTED_BY) VALUES ('Apasthamba Soothram', 1, 1 ,2);
+INSERT INTO soothram_master (soothram_name, approved, APPROVED_BY, REQUESTED_BY) VALUES ('Thrahyayana Soothram', TRUE, 1 ,2);
+INSERT INTO soothram_master (soothram_name, approved, APPROVED_BY, REQUESTED_BY) VALUES ('Bhodhayana Soothram', FALSE, 1 ,2);
+INSERT INTO soothram_master (soothram_name, approved, APPROVED_BY, REQUESTED_BY) VALUES ('Apasthamba Soothram', TRUE, 1 ,2);
 
-INSERT INTO gothram_master (gothram_name, approved, APPROVED_BY, REQUESTED_BY) VALUES ('Bharadwaja Gothram', 1, 1, 2);
-INSERT INTO gothram_master (gothram_name, approved, APPROVED_BY, REQUESTED_BY) VALUES ('Athreya Gothram', 1, 1, 2);
-INSERT INTO gothram_master (gothram_name, approved, APPROVED_BY, REQUESTED_BY) VALUES ('Vathula- Gothram', 0, 1, 2);
+INSERT INTO gothram_master (gothram_name, approved, APPROVED_BY, REQUESTED_BY) VALUES ('Bharadwaja Gothram', TRUE, 1, 2);
+INSERT INTO gothram_master (gothram_name, approved, APPROVED_BY, REQUESTED_BY) VALUES ('Athreya Gothram', TRUE, 1, 2);
+INSERT INTO gothram_master (gothram_name, approved, APPROVED_BY, REQUESTED_BY) VALUES ('Vathula- Gothram', FALSE, 1, 2);
 
 INSERT INTO status_master (status_master_id, status_master_name) VALUES (1, 'Active');
 INSERT INTO status_master (status_master_id, status_master_name) VALUES (2, 'Inactive');
